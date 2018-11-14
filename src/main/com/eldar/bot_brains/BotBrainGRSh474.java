577233056752d727e6f9f3055407fd16fbd78932
Nikol <44338413+Guroshkan@users.noreply.github.com>
2018-11-14 14:29:21 +0300

    Update BotBrainGRSh474.java

--- src/main/com/eldar/bot_brains/BotBrainGRSh474.java
+++ src/main/com/eldar/bot_brains/BotBrainGRSh474.java
@@ -1,55 +1,44 @@
-package com.eldar.bot_brains;
-
-
-import com.eldar.Bot;
-import static com.eldar.bot_brains.BotBrain.DOWN;
-import static com.eldar.bot_brains.BotBrain.LEFT;
-import static com.eldar.bot_brains.BotBrain.RIGHT;
-import static com.eldar.bot_brains.BotBrain.UP;
-import java.util.ArrayList;
-import java.util.List;
+package main.com.eldar.bot_brains;
+import main.com.eldar.Bot;
 import java.util.Random;
 
 public class BotBrainGRSh474 implements BotBrain {
-
-  private List<int[]> directions;
   private Bot bot;
+  DirectionOfBot Direction;
 
   public BotBrainGRSh474(){
-    directions = new ArrayList<>(5);
-    directions.add(RIGHT);
-    directions.add(DOWN);
-    directions.add(LEFT);
-    directions.add(UP);
-    directions.add(RIGHT);
+   Direction = new DirectionOfBot(0);
   }
 
   public void solveMaze(Bot curBot){
     this.bot=curBot;
-    keep_to_the_left_side();//обход лабиринта по левой стороне
+    keep_to_the_right_side();//обход лабиринта по правой стороне
     return;
   }
-  private void keep_to_the_left_side()
+  
+  private void keep_to_the_right_side()
   {
-      int dir = 0;
       while(!bot.isFinished()){
       for(;;){
-          if(bot.checkDir(directions.get(dir))&&!bot.checkDir(directions.get(dir+1))){
-          bot.moveInDir(directions.get(dir));
+          if(bot.checkDir((Direction.get()))&&!bot.checkDir(Direction.get_clockwise_rot())){
+          bot.moveInDir(Direction.get());
+          if(bot.isFinished())break;
           }
-          else if(bot.checkDir(directions.get(dir+1))){
-              if(dir!=3)dir+=1;
-              else dir = 0;
-              bot.moveInDir(directions.get(dir));
+          else if(bot.checkDir(Direction.get_clockwise_rot())){
+              Direction.set_clockwise_rot();
+              bot.moveInDir(Direction.get());
               break;
             }
-          else {
-              if(dir!=0)dir-=1;
-              else dir = 3;
-              bot.moveInDir(directions.get(dir));
+          else if(bot.checkDir(Direction.get_counterclockwise_rot())){
+              Direction.set_counterclockwise_rot();
+              bot.moveInDir(Direction.get());
               break;
             }
+          else{
+          Direction.reverse_bot();
+          bot.moveInDir(Direction.get());
+          }
         }  
-    }
-  }
+      }
+   }
 }
